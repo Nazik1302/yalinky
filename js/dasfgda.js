87ed3d41ea1ce929e467e08ce5782c8ee2a48288
HlovaOleh <hlovaoleh0@gmail.com>
2021-11-25 10:38:42 +0200

    place tree and other fix

---
+++ js/dasfgda.js
@@ -0,0 +1,11 @@
+const personalDataArray = [
+ ["first_name", "Ivan"],
+ ["last_name", "Ivanov"],
+ ["birth_date", "1995-12-12"],
+ ["gender", "male"],
+ ["marital_status", "single"]
+]
+
+const/let personalDataObj = ?
+
+{"first_name":"Ivan","last_name":"Ivanov","birth_date":"1995-12-12","gender":"male","marital_status":"single"}
